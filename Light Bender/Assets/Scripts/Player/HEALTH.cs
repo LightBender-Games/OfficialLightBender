3c3b67ce2e7170b7d07181ab52dc6887b3b66bc8
Mathis <mathis.rabouille@hotmail.fr>
2021-04-22 21:31:23 +0200

    Backup

---
+++ Light Bender/Assets/Scripts/Player/HEALTH.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Photon.Pun;
+using UnityEngine;
+
+public class HEALTH : MonoBehaviourPunCallbacks, IPunObservable,IDamageable
+{
+    public float health = 100;
+    Renderer[] visuals;
+    int team = 0;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) // recupere la health
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }
+        else
+        {
+            health = (float) stream.ReceiveNext();
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+    }
+
+    IEnumerator Respawn()
+    {
+        SetRenderers(false);
+        Debug.Log("2");
+        health = 100;
+        Debug.Log("1");
+        GetComponent<PlayerController>().enabled = false;
+        Debug.Log("mort 23 ");
+        Transform spawn = SpawnManager.instance.GetTeamSpawn(team);
+        transform.position = spawn.position;
+        transform.rotation = spawn.rotation;
+        GetComponent<PlayerController>().enabled = true;
+        yield return new WaitForSeconds(1);        
+        SetRenderers(true);
+    }
+
+    void SetRenderers(bool state)
+    {
+        foreach (var renderer in visuals)
+        {
+            renderer.enabled = state;
+        }
+    }
+
+    void Start()
+    {
+        visuals = GetComponentsInChildren<Renderer>();
+        team = (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"];
+    }
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("mort");
+            if (photonView.IsMine)
+            {
+                
+                Debug.Log("mort2");
+               StartCoroutine(Respawn());
+            }
+        }
+    }
+}
---
+++ Light Bender/Assets/Scripts/Weapons and elements/SingleShot.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class SingleShot : GUN
+{
+   public float firerate = 15; 
+   [SerializeField] Camera cam;
+   public LayerMask ignoreLayerMask;
+   public Transform gunTransform;
+   public AudioSource audio;
+
+   private float nexttimetofire = 0;
+
+   public  ParticleSystem particleSystem;
+
+   public float impactforce = 60;
+   
+    PhotonView Phv;
+    void Awake()
+    {
+       Phv = GetComponent<PhotonView>();
+       
+    }
+   public override void Use()
+   {
+      Phv.RPC("RPC_Shoot",RpcTarget.All);
+   }
+
+   //void Shoot()
+  // {
+     /* Ray rayon = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+      rayon.origin = cam.transform.position;
+      if (Time.time >= nexttimetofire)
+      {
+         particleSystem.Play();
+         nexttimetofire = Time.time + 1f/firerate;
+         if (Physics.Raycast(rayon, out RaycastHit hit))                //if (Physics.Raycast(rayon, out RaycastHit hit),100f,~ignoreLayerMask)
+         {
+            hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)iteminfo).damage);
+            if (hit.rigidbody != null)
+            {
+               hit.rigidbody.AddForce(-hit.normal * impactforce);
+            }
+            Pv.RPC("RPC_Shoot",RpcTarget.All,hit.point,hit.normal);
+         }*/
+      
+           // Phv.RPC("RPC_Shoot",RpcTarget.All);
+  // }
+
+   [PunRPC]
+   void RPC_Shoot()
+   {
+      
+     /* Ray rayon = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+      rayon.origin = cam.transform.position;*/
+     //if (!Phv.IsMine)
+      //  return;
+     Ray rayon = new Ray(gunTransform.position, gunTransform.forward);
+      if (Time.time >= nexttimetofire)
+      {
+         particleSystem.Play();
+         audio.Play();
+         nexttimetofire = Time.time + 1f / firerate;
+         if (Physics.Raycast(rayon, out RaycastHit hit, 100f, ~ignoreLayerMask))
+        // if (Physics.Raycast(rayon, out RaycastHit hit),100f,~ignoreLayerMask)
+         {
+           
+            Collider[] bimp = Physics.OverlapSphere(hit.point, 0.3f);
+            if (bimp.Length != 0)
+            {
+               GameObject buletimpact = Instantiate(bulletImpactPrefab, hit.point + hit.normal * 0.001f,
+                  Quaternion.LookRotation(hit.normal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+               Destroy(buletimpact, 2f);
+               buletimpact.transform.SetParent(bimp[0].transform);
+            }
+            // hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo) iteminfo).damage);
+            if (hit.rigidbody != null)
+            {
+               hit.rigidbody.AddForce(-hit.normal * impactforce);
+            }
+           var enemyhealth = hit.collider.GetComponent<HEALTH>();
+          //  Debug.Log(enemyhealth);
+           // Debug.Log(hit.collider.name);
+            if (enemyhealth)
+            {
+               enemyhealth.TakeDamage(((GunInfo) iteminfo).damage);
+               Debug.Log("hit");
+            }
+         }
+      }
+   }
+}
